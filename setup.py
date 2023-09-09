1e5db05cefbcaa4fc364cdda643dd9a83a8de30c
ronen4822 <ronen4822@gmail.com>
2023-09-09 23:21:03 +0300

    -add very basic kube interaction

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(
     name="worker_manager",
     version="0.0.1",
     packages=find_packages(),
-    install_requires=['python-socketio', 'aioboto3', 'jsonschema', 'websocket-client', 'psutil']  # Todo: load requirements.txt
+    install_requires=['python-socketio', 'aioboto3', 'jsonschema', 'websocket-client', 'psutil', 'kubernetes','qemu.qmp']  # Todo: load requirements.txt
 )
