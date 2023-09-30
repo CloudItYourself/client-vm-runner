3aaf73e90e025a11d4b762f7cae66f64de9ec0d7
ronen4822 <ronen4822@gmail.com>
2023-10-01 01:45:05 +0300

    -more good stuff

--- internal_controller/kubernetes_handling/kube_utility_installation_functions.py
+++ internal_controller/kubernetes_handling/kube_utility_installation_functions.py
@@ -3,3 +3,7 @@ import os
 
 def install_k3s() -> bool:
     return os.system('curl -sfL https://get.k3s.io | sh - ') == 0
+
+
+def install_metrics_server() -> bool:
+    return os.system('kubectl apply -f /usr/src/kube-deployments/metrics-server.yaml') == 0
