2000046d8ad1e2711c98c203753b64c9f5183347
ronen4822 <ronen4822@gmail.com>
2024-02-05 11:57:55 +0200

    -unretard myself

--- internal_controller/connection/vm_manager_connection.py
+++ internal_controller/connection/vm_manager_connection.py
@@ -73,9 +73,9 @@ class ConnectionHandler:
         os.environ['INVOCATION_ID'] = ""
         os.system('/usr/local/bin/k3s-agent-uninstall.sh')
 
-        agent_installation_command = (f'INSTALL_K3S_EXEC="agent --token {registration_details.k8s_token}'
+        agent_installation_command = (f'INSTALL_K3S_SKIP_DOWNLOAD=true INSTALL_K3S_EXEC="agent --token {registration_details.k8s_token}'
          f' --server https://{registration_details.k8s_ip}:{registration_details.k8s_port}'
-         f'--node-name {node_name} --kubelet-arg cgroups-per-qos=false --kubelet-arg enforce-node-allocatable='
+         f' --node-name {node_name} --kubelet-arg cgroups-per-qos=false --kubelet-arg enforce-node-allocatable='
          f' --vpn-auth-file={vpn_file.absolute()}" /usr/local/src/install.sh')
         return os.system(agent_installation_command) == 0
 
